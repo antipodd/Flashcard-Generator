4b6c6189d36c7432cefeede672de771dae3a65a8
Karel <karel.hartlieb@gmail.com>
2017-04-10 15:17:22 -0500

    commit 041017

---
+++ flashcards.js
@@ -0,0 +1,51 @@
+//scope-safe constructor for basic flash cards
+function BasicCard(front, back) { 
+  if (!(this instanceof BasicCard)) { 
+    return new BasicCard(front, back);
+  }
+  this.front = front;
+  this.back = back;
+}
+//scope-safe constructor for cloze flash cards
+function ClozeCard(text, cloze) { 
+  if (!(this instanceof ClozeCard)) { 
+    return new ClozeCard(text, cloze);
+  }
+  this.text = text;
+  this.cloze = cloze;
+}
+
+//prototype function to return the cloze portion of the flash card to the console
+ClozeCard.prototype.clozeText = function() {
+	return this.cloze;
+}
+
+//prototype function to return the partial text portion of the flash card to the console
+ClozeCard.prototype.partial = function() {
+	var completeText = this.text;
+	if (completeText.includes(this.cloze)) {
+		return completeText.replace(this.cloze, "...");
+	} else {
+		var error = "Cloze not found in text, try again"
+		return error;
+	}
+}
+
+//prototype function to return the full text of the flash card to the console
+ClozeCard.prototype.fullText = function() {
+	return this.text;
+}
+
+var firstPresident = BasicCard("Who was the first president of the United States?", "George Washington");
+
+var firstPresidentCloze = ClozeCard("George Washington was the first president of the United States.", "George Washington");
+
+console.log(firstPresidentCloze.fullText());
+
+console.log(firstPresidentCloze.clozeText());
+
+console.log(firstPresidentCloze.partial());
+
+var brokenCloze = ClozeCard("This doesn't work", "oops");
+
+console.log(brokenCloze.partial());
